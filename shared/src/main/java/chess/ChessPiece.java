0bd36291901d60504178f75fa4b6346931046a55
jasonsummers012 <jasonsummers012@gmail.com>
2025-05-05 14:37:49 -0600

    Completed board tests, so all tests are done!

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -511,4 +511,12 @@ public class ChessPiece {
     public int hashCode() {
         return Objects.hash(color, type);
     }
+
+    @Override
+    public String toString() {
+        return "ChessPiece{" +
+                "color=" + color +
+                ", type=" + type +
+                '}';
+    }
 }
