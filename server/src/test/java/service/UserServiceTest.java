c2c3b1981b8288a3e1127ab449494e1c7ff909c1
jasonsummers012 <jasonsummers012@gmail.com>
2025-05-20 13:44:11 -0600

    Wrote and passed register tests, starting Login endpoint

--- server/src/test/java/service/UserServiceTest.java
+++ server/src/test/java/service/UserServiceTest.java
@@ -1,8 +1,14 @@
 package service;
 
+import dataaccess.DataAccessException;
 import dataaccess.UserDAO;
+import handler.request.RegisterRequest;
+import handler.result.RegisterResult;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 
 public class UserServiceTest {
     private UserDAO testDAO;
@@ -14,4 +20,22 @@ public class UserServiceTest {
         userService = new UserService(testDAO);
     }
 
+    @Test
+    public void testRegisterNewUser() throws DataAccessException {
+        RegisterRequest testRequest = new RegisterRequest("Jeremy", "12345", "jeremy@emila.com");
+        RegisterResult testResult = userService.register(testRequest);
+        assertNotNull(testResult);
+        assertEquals("Jeremy", testResult.username());
+    }
+
+    @Test
+    public void testRegisterRepeatUser() throws DataAccessException {
+        RegisterRequest testRequest = new RegisterRequest("Jeremy", "12345", "jeremy@email.com");
+        RegisterResult testResult = userService.register(testRequest);
+
+        RegisterRequest repeatRequest = new RegisterRequest("Jeremy", "12345", "jeremy@email.com");
+        assertThrows(DataAccessException.class, () -> {
+            userService.register(repeatRequest);
+        });
+    }
 }
