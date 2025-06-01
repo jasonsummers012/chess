e844c07fbea48d994133c77d339e372e9063abb2
jasonsummers012 <jasonsummers012@gmail.com>
2025-06-01 14:58:44 -0600

    Started implementing Repl

--- client/src/main/java/ui/Repl.java
+++ client/src/main/java/ui/Repl.java
@@ -1,4 +1,22 @@
 package ui;
 
+import java.util.Scanner;
+
 public class Repl {
+    private final PreLoginClient preLoginClient;
+    private final PostLoginClient postLoginClient;
+
+    public Repl(String serverUrl) {
+        preLoginClient = new PreLoginClient(serverUrl);
+        postLoginClient = new PostLoginClient(serverUrl, null);
+    }
+
+    public void run() {
+        System.out.println(preLoginClient.help());
+
+        Scanner scanner = new Scanner(System.in);
+        var result = "";
+        while (!result.equals("quit")) {
+        }
+    }
 }
